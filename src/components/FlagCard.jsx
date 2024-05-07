20e4a0769f8532a502316f50bbe4b53ef1bea2a2
Abhishek Patil <abishkpatil@gmail.com>
2024-05-08 01:13:08 +0530

    fix: add alt to img tag

--- src/components/FlagCard.jsx
+++ src/components/FlagCard.jsx
@@ -5,7 +5,7 @@ export const FlagCard = ({ name, url }) => {
     return <>
         <Box sx={{ border: '1px #DADADA solid', borderRadius: '4px' }}>
             <Stack spacing={1} display={"flex"} justifyContent={"center"} textAlign={"c"} sx={{ padding: '10px', textAlign:'center' }}>
-                <img src={url} alt="image" height={"100px"}/>
+                <img src={url} alt={name} height={"100px"}/>
                 <span>{name}</span>
             </Stack>
         </Box>
