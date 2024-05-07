7f60f63ba281062eeb82fdee605a285f2bd5db91
Abhishek Patil <abishkpatil@gmail.com>
2024-05-08 00:11:06 +0530

    feat: add flags API call

--- src/components/FlagsGrid.jsx
+++ src/components/FlagsGrid.jsx
@@ -1,6 +1,22 @@
 import Grid from '@mui/material/Grid';
+import axios from "axios"
+import { useState, useEffect } from 'react';
 
 export const FlagsGrid = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const getData = async () => {
+            const url = "https://restcountries.com/v3.1/all";
+            try {
+                const res = await axios.get(url);
+                console.log("name: ", res.data[0].name.official)
+                console.log("flag src: ", res.data[0].flags.png)
+            } catch (e) {
+                console.log("err:", e.response);
+            }
+        }
+        getData();
+    }, [])
     return <>
         <Grid container spacing={2}>
             <Grid item xs={12} sm={6} md={4} lg={2}>
